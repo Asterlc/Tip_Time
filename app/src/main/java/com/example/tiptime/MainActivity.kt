647fb49152605b69e20d4facb5fcf78e470adfb3
Lucas Aster <asterlc102@gmail.com>
2022-08-30 21:05:39 -0300

    Fixed warnings

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -8,26 +8,24 @@ import kotlin.math.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.btnCalculate.setOnClickListener { calculateTip() }
     }
 
     private fun calculateTip() {
         val cost = binding.costOfService.text.toString().toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        val tipPercentage: Double = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.btn_Radio_good -> 0.18
             R.id.btn_Radio_regular -> 0.15
             else -> 0.20
         }
 
-        var tip = tipPercentage * cost
+        var tip: Double = tipPercentage * cost
         val roundUp = binding.switchRoundTip.isChecked
         if (roundUp) {
             tip = ceil(tip)
